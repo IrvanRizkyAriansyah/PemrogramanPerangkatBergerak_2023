49f0c5b0b0aa9ec3d27b075759c3b16fef3e062f
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2023-03-28 08:04:30 +0700

    Pertemuan-4

--- pertemuan-3/App.js
+++ pertemuan-3/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { ItemTypograp } from "./src/component/ItemTypograp";
+import { ImageItem } from "./src/component/Imageitem";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden={true} />
+      <View style={styles.container}>
+        <ItemTypograp />
+        <ImageItem />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
